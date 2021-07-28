300eb6ba467379991b05f8ae51fb648187a9deee
Priyanshi Soni <priyanshisoni244@gmail.com>
2021-07-28 10:46:05 +0530

    "changes4"

---
+++ Gittest1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gittest1
+{
+    class Class1
+    {
+        private static void display()
+        {
+            Console.WriteLine("Chhanges made in git");
+        }
+    }
+}
+}
--- Gittest1/Program.cs
+++ Gittest1/Program.cs
@@ -10,14 +10,13 @@ namespace Gittest1
             Console.WriteLine("Hello World!");
             Console.WriteLine("H1!");
             Console.WriteLine("Hello Priyanshi!");
-            private static void display()
-            {
-                Console.WriteLine("Chhanges made in git");}
-        }
+           
 
         private static void Display()
         {
             Console.WriteLine("This is method Changed in local file");
                     }
+
+        
     }
 }
